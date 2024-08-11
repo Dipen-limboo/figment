0fc55504d303d61b35e5b264f03d5e2b7231f1a9
dipen <dipenlimboo0564@gmail.com>
2024-08-11 00:59:39 -0700

    designing

--- js/product.js
+++ js/product.js
@@ -2,17 +2,15 @@ document.addEventListener('DOMContentLoaded', function() {
     const containers = document.querySelectorAll('.product-view-container');
     const colorDivs = document.querySelectorAll('.color-div img');
     const varientDivs = document.querySelectorAll('.varient-div img');
-    let defaultSrc = '../images/tshirt.jfif'; // Default image source
+    let defaultSrc = '../images/tshirt.jfif'; 
     let currentSrc = defaultSrc;
 
-    // Show the first product-view-container (white by default)
     containers[0].classList.add('active');
 
-    // Change the main product image when a color-div is clicked
     colorDivs.forEach(function(colorDiv) {
         colorDiv.addEventListener('click', function() {
             const clickedSrc = this.src;
-            currentSrc = clickedSrc; // Update the current image source
+            currentSrc = clickedSrc; 
 
             containers.forEach(function(container) {
                 const mainImage = container.querySelector('.product-view-image img');
@@ -25,17 +23,16 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-    // Change the main product image on hover over a varient-div
     varientDivs.forEach(function(varientDiv) {
         varientDiv.addEventListener('mouseenter', function() {
             const hoveredSrc = this.src;
             const activeContainer = document.querySelector('.product-view-container.active .product-view-image img');
-            activeContainer.src = hoveredSrc; // Temporarily change the image
+            activeContainer.src = hoveredSrc; 
         });
 
         varientDiv.addEventListener('mouseleave', function() {
             const activeContainer = document.querySelector('.product-view-container.active .product-view-image img');
-            activeContainer.src = currentSrc; // Revert to the previously selected image
+            activeContainer.src = currentSrc; 
         });
     });
 });
